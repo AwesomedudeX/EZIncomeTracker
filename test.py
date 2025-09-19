fb7e50f5ee1d1498b472bda3e5b8f73d18190ae2
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2025-09-19 12:43:23 -0600

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import py7zr as zip
-
-print("Extracting...")
-zip.SevenZipFile("C:/Users/techn/Desktop/STALKER/Compressed Files/RTac GAMMA Large Mods.7z", "r").extractall("C:/Users/techn/Desktop/STALKER/Compressed Files")
-print("Done!")
